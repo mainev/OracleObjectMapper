0fd9cd9f69aeea2dda0b1a637801bab47f3ba102
mainev <mainevillarias@gmail.com>
2019-04-25 10:43:36 -0500

    wpf ui

---
+++ DbCollections.cs
@@ -0,0 +1,81 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS.DatabaseConnections.Oracle
+{
+    public class DbCollections<T> where T : IDbObject, new()
+    {
+
+        private string dataSource;
+        private string tableName;
+
+        public DbCollections(string dataSource)
+        {
+            this.dataSource = dataSource;
+            this.tableName = GetTableName(typeof(T));
+        }
+
+        public List<T> Get()
+        {
+            using (OracleConnection oracleConnection = new OracleConnection(this.dataSource))
+            {
+                string sql = "SELECT * FROM " + this.tableName;
+                DbMapper<T> dbMapper = new DbMapper<T>();
+
+                OracleCommand cmd = oracleConnection.CreateCommand();
+                oracleConnection.Open();
+                cmd.CommandText = sql;
+                OracleDataReader dr = cmd.ExecuteReader();
+                List<T> objectList = dbMapper.GetDataObjects(dr);
+                oracleConnection.Close();
+
+                return objectList;
+            }
+        }
+
+        public T Add(T item)
+        {
+            using (OracleConnection oracleConnection = new OracleConnection(this.dataSource))
+            {
+                oracleConnection.Open();
+                OracleCommand cmd = oracleConnection.CreateCommand();
+                cmd.CommandText = new DbMapper<T>().CreateInsertSQL(item, this.tableName);
+                int rowsUpdated = cmd.ExecuteNonQuery();
+                oracleConnection.Close();
+            }
+            return item;
+        }
+
+        public T Update(T item)
+        {
+            using (OracleConnection oracleConnection = new OracleConnection(this.dataSource))
+            {
+                oracleConnection.Open();
+                OracleCommand cmd = oracleConnection.CreateCommand();
+                cmd.CommandText = new DbMapper<T>().CreateUpdateSQL(item, this.tableName);
+                int rowsUpdated = cmd.ExecuteNonQuery();
+                oracleConnection.Close();
+            }
+            return item;
+        }
+
+        private string GetTableName(System.Type type)
+        {
+            System.Attribute[] attrs = System.Attribute.GetCustomAttributes(type);
+            foreach (System.Attribute attr in attrs)
+            {
+                if (attr is DbObject)
+                {
+                    DbObject db = (DbObject)attr;
+                    return db.table;
+                }
+            }
+            throw new Exception(type.FullName + " is not a valid implementation of DbObject");
+        }
+
+    }
+}
---
+++ DbMapper.cs
@@ -0,0 +1,180 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS.DatabaseConnections.Oracle
+{
+    public class DbMapper<T> where T : IDbObject, new()
+    {
+        private List<String> dbFields = new List<String>();
+
+        public DbMapper()
+        {
+        }
+
+        private void setDbFields(OracleDataReader reader)
+        {
+            int i = 0;
+            while (i < reader.FieldCount)
+            {
+                dbFields.Add(reader.GetName(i).ToLower());
+                i++;
+            }
+        }
+
+        private T GetInstance()
+        {
+            return new T();
+        }
+
+        public string CreateUpdateSQL(T item, string tbl)
+        {
+
+            List<string> values = new List<string>();
+            int id = 0;
+            var dbObject = this.GetInstance();
+            Type dbObjectType = typeof(T);
+            var fieldNames = dbObjectType.GetProperties()
+                           .ToList();
+            //var fieldNames = dbObjectType.GetProperties()
+            //                .Select(field => field.Name)
+            //                .ToList();
+
+            fieldNames.ForEach(field =>
+            {
+                //FieldInfo fi = dbObjectType.GetField(field, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+                //PropertyInfo fi = dbObjectType.GetProperty(field);
+
+                string value = "";
+                if (String.Equals("ID".ToLower(), field.Name.ToLower()))
+                {
+                    id = Convert.ToInt32(field.GetValue(item));
+                }
+                else
+                {
+                    switch (field.PropertyType.Name)
+                    {
+                        case "Boolean":
+                            value = (string)((bool)field.GetValue(item) ? "1" : "0");
+                            break;
+                        case "String":
+                            value = "\'" + (string)(field.GetValue(item)) + "\'";
+                            break;
+                        case "Int16":
+                        case "Int32":
+                            value = Convert.ToString(field.GetValue(item));
+                            break;
+                        default:
+                            throw new NotImplementedException("Field " + field + " " + field.PropertyType.Name + " is not filtered");
+                    }
+                    value = String.IsNullOrEmpty(value) ? "null" : value;
+
+                    string field_value = "";
+                    field_value += field + "=" + value;
+                    values.Add(field_value);
+                }
+            });
+
+            return String.Format("UPDATE {0} SET {1} WHERE ID = {2}", tbl, string.Join(",", values), id);
+
+
+        }
+
+        public string CreateInsertSQL(T item, string tbl)
+        {
+
+            List<string> values = new List<string>();
+
+            var dbObject = this.GetInstance();
+            Type dbObjectType = typeof(T);
+            var fieldNames = dbObjectType.GetProperties().ToList();
+            //var fieldNames = dbObjectType.GetFields()
+            //                .Select(field => field.Name)
+            //                .ToList();
+
+            fieldNames.ForEach(field =>
+            {
+                //FieldInfo fi = dbObjectType.GetField(field, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+                string value = "";
+                //if (fi != null)
+                //{
+                    switch (field.PropertyType.Name)
+                    {
+                        case "Boolean":
+                            value = (string)((bool)field.GetValue(item) ? "1" : "0");
+                            break;
+                        case "String":
+                            value = "\'" + (string)(field.GetValue(item)) + "\'";
+                            break;
+                        case "Int16":
+                        case "Int32":
+                            value = Convert.ToString(field.GetValue(item));
+                            break;
+                        default:
+                            throw new NotImplementedException("Field " + field + " " + field.PropertyType.Name + " is not filtered");
+                    }
+                    value = String.IsNullOrEmpty(value) ? "null" : value;
+                    values.Add(value);
+                //}
+            });
+
+            return String.Format("INSERT INTO {0} ({1}) VALUES ({2})", tbl, string.Join(",", fieldNames), string.Join(",", values));
+
+        }
+
+        public T GetDataObject()
+        {
+            var dbObject = this.GetInstance();
+
+            return dbObject;
+        }
+
+        public List<T> GetDataObjects(OracleDataReader reader)
+        {
+
+            List<T> list = new List<T>();
+            setDbFields(reader);
+
+            while (reader.Read())
+            {
+                var dbObject = this.GetInstance();
+                Type dbObjectType = typeof(T);
+
+                dbFields.ForEach(field_name =>
+                {
+                    //FieldInfo fi = dbObjectType.GetField(field_name);
+                    PropertyInfo fi = dbObjectType.GetProperty(field_name);
+
+                    if (fi != null && reader[field_name] != System.DBNull.Value)
+                    {
+                        switch (fi.PropertyType.Name)
+                        {
+                            case "Boolean":
+                                fi.SetValue(dbObject, Convert.ToBoolean(reader[field_name]));
+                                break;
+                            case "String":
+                                fi.SetValue(dbObject, reader[field_name]);
+                                break;
+                            case "Int16":
+                                fi.SetValue(dbObject, Convert.ToInt16(reader[field_name]));
+                                break;
+                            case "Int32":
+                                fi.SetValue(dbObject, Convert.ToInt32(reader[field_name]));
+                                break;
+                            default:
+                                throw new NotImplementedException("Field " + field_name + " " + fi.PropertyType.Name + " is not configured in DbMapper");
+
+                        }
+                    }
+                });
+                list.Add(dbObject);
+            }
+
+            return list;
+        }
+    }
+}
